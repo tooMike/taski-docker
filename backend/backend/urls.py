0d48e80111f38ea09108bcbaccf9c70f4dba2865
Михаил Беляев <belyaev-m-o@yandex.ru>
2024-06-20 16:04:20 +0200

    Pytest + DockerHub Final + Server + Bot

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -1,12 +1,47 @@
-from api import views
+from django.conf.urls import url
 from django.contrib import admin
 from django.urls import include, path
-from rest_framework import routers
+from drf_yasg import openapi
+from drf_yasg.views import get_schema_view
+from rest_framework import permissions, routers
+
+from api import views
 
 router = routers.DefaultRouter()
-router.register('tasks', views.TaskView, 'task')
+router.register("tasks", views.TaskView, "task")
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api/', include(router.urls)),
+    path("admin/", admin.site.urls),
+    path("api/", include(router.urls)),
+]
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Cats API",
+        default_version="v1",
+        description="Документация для приложения cats проекта Kittygram",
+        # terms_of_service="URL страницы с пользовательским соглашением",
+        contact=openapi.Contact(email="admin@kittygram.ru"),
+        license=openapi.License(name="BSD License"),
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
+
+urlpatterns += [
+    url(
+        r"^swagger(?P<format>\.json|\.yaml)$",
+        schema_view.without_ui(cache_timeout=0),
+        name="schema-json",
+    ),
+    url(
+        r"^swagger/$",
+        schema_view.with_ui("swagger", cache_timeout=0),
+        name="schema-swagger-ui",
+    ),
+    url(
+        r"^redoc/$",
+        schema_view.with_ui("redoc", cache_timeout=0),
+        name="schema-redoc",
+    ),
 ]
